5be0f983575bde34a013454486c1032c0c79a010
Alan McCabe <alan.mccabe@ge.com>
2020-06-14 20:05:45 -0400

    Second push

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,6 +7,9 @@ var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes
 var button = d3.select("#filter-btn");
 var inputField1 = d3.select("#datetime");
 var inputField2 = d3.select("#city");
+var inputField3 = d3.select("#state");
+var inputField4 = d3.select("#country");
+var inputField5 = d3.select("#shape");
 
 var populate = (dataInput) => {
   dataInput.forEach(ufo_sightings => {
@@ -22,22 +25,28 @@ button.on("click", () => {
   d3.event.preventDefault();
   var inputDate = inputField1.property("value").trim();
   var inputCity = inputField2.property("value").toLowerCase().trim();
+  var inputState = inputField3.property("value").toLowerCase().trim();
+  var inputCountry = inputField4.property("value").toLowerCase().trim();
+  var inputShape = inputField5.property("value").toLowerCase().trim();
 
   var filterDate = data.filter(data => data.datetime === inputDate);
   var filterCity = data.filter(data => data.city === inputCity);
-  var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+  var filterState = data.filter(data => data.state === inputState);
+  var filterCountry = data.filter(data => data.country === inputCountry);
+  var filterShape = data.filter(data => data.shape === inputShape);
+  var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity && data.state === inputState && data.country === inputCountry && data.shape === inputShape);
 
   tbody.html("");
 
   let response = {
-    filterData, filterCity, filterDate
+    filterData, filterCity, filterState, filterCountry, filterShape, filterDate
   }
 
   if (response.filterData.length !== 0) {
     populate(filterData);
   }
-    else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-      populate(filterCity) || populate(filterDate);
+    else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterState.length !== 0 || response.filterCountry.length !== 0 || response.filterShape.length !== 0))){
+      populate(filterCity) || populate(filterDate) || populate(filterState) || populate(filterCountry) || populate(filterShape);
   
     }
     else {
